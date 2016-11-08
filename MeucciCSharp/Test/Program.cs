81c0f7975629ca929033a1d65873683264da2186
Trader 42 <github@42capital.fr>
2016-11-08 08:06:30 +0100

    Test executable

---
+++ MeucciCSharp/Test/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using Qutes.Moments;
+
+namespace Test
+{
+    class Program
+    {
+        struct Moments
+        {
+            public double StdDev;
+            public double Skewness;
+            public double Kurtosis;
+        }
+
+        static void Main(string[] args)
+        {
+            double[] spotPrices = null;
+            using (var textReader= File.OpenText("SPX.csv"))
+            {
+                var csv = new CsvReader(textReader);
+                spotPrices = csv.GetRecords<double>().ToArray();
+            }
+
+            var dailyReturns = new double[spotPrices.Length - 1];
+            var monthlyReturns = new double[spotPrices.Length - 21];
+            var annualReturns = new double[spotPrices.Length - 252];
+
+            for (int i = 1; i < spotPrices.Length; ++i)
+                dailyReturns[i - 1] = Math.Log(spotPrices[i]/spotPrices[i - 1]);
+
+            for (int i = 21; i < spotPrices.Length; ++i)
+                monthlyReturns[i - 21] = Math.Log(spotPrices[i] / spotPrices[i - 21]);
+
+            for (int i = 252; i < spotPrices.Length; ++i)
+                annualReturns[i - 252] = Math.Log(spotPrices[i] / spotPrices[i - 252]);
+
+            var nMoments = 4;
+            //Projection horizon
+            var kDailyAnnual = 252;
+            var kDailyMonthly = 21;
+            var kMonthlyAnnual = 12;
+
+            //Compute Moments
+            var dailyMoms = qMoments.SummStats(dailyReturns, nMoments);
+            var monthlyMoms = qMoments.SummStats(monthlyReturns, nMoments);
+            var annualMoms = qMoments.SummStats(annualReturns, nMoments);
+
+
+            //compute single - period non - central moments
+            var mu_daily = qMoments.Central2Raw(dailyMoms.Item1);
+            var mu_monthly = qMoments.Central2Raw(monthlyMoms.Item1);
+
+            //compute single - period cumulants
+            var ka_daily = qMoments.Raw2Cumul(mu_daily);
+            var ka_monthly = qMoments.Raw2Cumul(mu_monthly);
+
+            //compute multi - period cumulants
+            var Ka_dailyMonthly = ka_daily.Select(x=>x*kDailyMonthly).ToArray();
+            var Ka_dailyAnnual = ka_daily.Select(x=>x* kDailyAnnual).ToArray();
+            var Ka_monthlyAnnual = ka_monthly.Select(x=> x*kMonthlyAnnual).ToArray();
+
+            //compute multi - period non - central moments
+            var Mu_DailyMonthly = qMoments.Cumul2Raw(Ka_dailyMonthly);
+            var Mu_DailyAnnual = qMoments.Cumul2Raw(Ka_dailyAnnual);
+            var Mu_MonthlyAnnual = qMoments.Cumul2Raw(Ka_monthlyAnnual);
+
+            //compute multi - period central moments
+            var MuDailyMonthly = qMoments.Raw2Central(Mu_DailyMonthly);
+            var MuDailyAnnual = qMoments.Raw2Central(Mu_DailyAnnual);
+            var MuMonthlyAnnual = qMoments.Raw2Central(Mu_MonthlyAnnual);
+
+            var standardisedDailyMonthly = qMoments.ComputeStandardizedStatistics(MuDailyMonthly);
+            var standardisedDailyAnnual = qMoments.ComputeStandardizedStatistics(MuDailyAnnual);
+            var standardisedMonthlyAnnual = qMoments.ComputeStandardizedStatistics(MuMonthlyAnnual);
+
+
+        }
+    }
+}
--- Moments/Matlab/S_ProjSummStats.m
+++ Moments/Matlab/S_ProjSummStats.m
@@ -4,19 +4,31 @@
 % see Meucci, A. (2010) "Annualization and General Projection of Skewness, Kurtosis and All Summary Statistics"
 % GARP Risk Professional, August, pp. 52-54 
 
+filename = '..\SPX_returns.csv';
+delimiterIn = ',';
+headerlinesIn = 1;
+spxRets = importdata(filename,delimiterIn,headerlinesIn);
+
+dailyRets = spxRets.data(:,1);
+weeklyRets = spxRets.data(:,2);
+annualRets = spxRets.data(:,3);
+
+weeklyRets(~any(~isnan(weeklyRets), 1),:)=[];
+
+
 clear; clc; close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % inputs
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-N=6;  % focus on first N standardized summary statistics
-K=100; % projection horizon
+N=4;  % focus on first N standardized summary statistics
+K=252; % projection horizon
 
 % generate arbitrary distribution
 J=100000;  % number of scenarios
 
 a=-1; % shift
-m=.2; % log-mean
-s=.4; %log-sdev
+m=.0; % log-mean
+s=.01; %log-sdev
 
 Z=randn(J/2,1); 
 Z=[Z;-Z]/std(Z,1);
